efb5d7c4811cbb551a83b5c5479093abff65de1f
Mykola Kanunnikov <kanunnikovmykola@gmail.com>
2022-11-19 02:06:43 +0200

    datateble. fixed recordId

--- force-app/main/default/lwc/quickAction/quickAction.js
+++ force-app/main/default/lwc/quickAction/quickAction.js
@@ -1,9 +1,18 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import showRooms from '@salesforce/apex/QuickActionController.showRooms';
 
 export default class QuickAction extends LightningElement {
     columns = [{ label: 'Rooms', fieldName: 'Name' }];
-    @api recordId;
-    @wire(showRooms, {recordId: '$recordId'})
-    record;
+    data;
+
+    _recordId;
+    @api set recordId(value) {
+        this._recordId = value;
+        this.data = showRooms({recordId: this._recordId});
+    }
+
+    get recordId() {
+        return this._recordId;
+    }
+
 }
